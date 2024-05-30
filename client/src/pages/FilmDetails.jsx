a3a70c52f25b931d8d84ca8af9b99bfb0a295a9c
RenanCampista <113597566+RenanCampista@users.noreply.github.com>
2024-05-30 20:01:26 -0300

    feat: adiciona pagina do filme com as informações dele
    Falta fazer a estilização da pagina

--- client/src/pages/FilmDetails.jsx
+++ client/src/pages/FilmDetails.jsx
@@ -1,15 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react'
+import Axios from 'axios'
 import { useParams } from 'react-router-dom';
 
 const FilmDetails = () => {
-  let { id } = useParams();
+  const { id } = useParams();
+  const [film, setFilm] = useState({})
+
+  useEffect(() => {
+    Axios.get(`http://localhost:3002/films/${id}`)
+    .then((res) => {
+      if (res.data) {
+        setFilm(res.data)
+      }else {
+        console.log(error);
+      }
+    })
+  },[])
+
+  function extractVideoId(url) {
+    var videoId = url.split('v=')[1];
+    var ampersandPosition = videoId.indexOf('&');
+    if(ampersandPosition != -1) {
+      videoId = videoId.substring(0, ampersandPosition);
+    }
+    return videoId;
+  }
 
 
   return (
-    <div>
-      <h1>Film Details</h1>
-      <p>ID: {id}</p>
+    <div className='movie-info'>
+      <div className='movie-poster'>
+        <div className='poster'>
+          <img src={film.poster} alt={film.name} />
+        </div>
+      </div>
+
+      <div className='movie-info'>
+        <h2>{film.name}</h2>
+        <p>Release Date: {film.release_date}</p>
+        <p>Duration: {film.duration} min</p>
+        <p>Director: {film.director}</p>
+        {/* <p>Genre: {film.move}</p> */}
+        <p>Age rating: {film.age_rating}</p>
+        <p>Synopsis: {film.synopsis}</p>
+        {/* <p>Rotten Tomatoes Score: {film.total_rating}</p> */}
+      </div>
+
+
+      <div className='movie-trailer'>
+      <iframe src={`https://www.youtube.com/embed/${extractVideoId(film.trailer)}`} title="Movie Trailer"></iframe>
     </div>
+
+
+    </div>
+    
   );
 }
 
