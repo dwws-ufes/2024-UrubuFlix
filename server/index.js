08ceb016070d6a530d77b47534e3c7af360d13b4
BrunoSantosFF <brunosantosfernandes0@gmail.com>
2024-05-30 23:06:34 -0300

    change files

--- server/index.js
+++ server/index.js
@@ -1,15 +1,18 @@
-import express from 'express'; // Importa o módulo express
-import cors from 'cors'; // Importa o módulo cors
+import express from 'express' 
+import cors from 'cors' 
 import bcrypt from 'bcrypt'
-import { PrismaClient } from '@prisma/client'
+import prisma from './services/prisma.js'
 import dotenv from 'dotenv'
 import jwt from 'jsonwebtoken'
 import nodemailer from 'nodemailer'
-import cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser'
+import fs from 'fs'
 
-
-
-const prisma = new PrismaClient()
+import * as userServices from './services/userServices.js';
+import * as catalogServices from './services/catalogServices.js';
+import * as movieServices from './services/movieServices.js';
+import * as genreENUM from './enum/genreENUM.js';
+import { exit } from 'process'
 
 dotenv.config()
 const app = express()
@@ -23,6 +26,10 @@ app.use(cors({
 
 const PORT = process.env.PORT || 3002;
 
+
+const filePathMoviesJSON = './script_movies/movies.json';// Caminho do arquivo JSON com os filmes FIQUE ATENTO PARA O CAMINHO CORRETO
+
+
 // Verifica se a conexão com o banco de dados foi estabelecida
 app.listen(PORT, async () => {
   console.log(`Server is running on port ${PORT}`);
@@ -35,183 +42,51 @@ app.listen(PORT, async () => {
   } 
   catch (error) {
     console.error('Error connecting to the database:', error);
+    exit(1);
+  }
+  try{
+    await genreENUM.initializeGenres();
+    console.log('Genres initialized');
+  }
+  catch (error) {
+    console.error('Error initializing genres:', error);
+  }
+  try{
+    await movieServices.initializeMovies(filePathMoviesJSON);
+  }
+  catch (error) {
+    console.error('Error initializing movies:', error);
   }
 });
 
 
 //Register 
 app.post('/register', async (req, res) => {
-  
-  const {username, email, password} = req.body;
-   
-  const user = await prisma.users.findFirst({
-    where: {
-      email:email
-    }
-  });
-  
-
-  if (user) {
-    console.log('user already existed');
-    return res.json({status: false,message: 'user already existed'})
-  }
-
-  if (!username || !email || !password) {
-    console.log('Por favor, preencha todos os campos');
-    return res.json({status: false,message: 'Empty campo'})
-  }
-  
-  const hashpassword = await bcrypt.hash(password,10)
-
-  try {
-    const x = await prisma.users.create({
-      data: {
-        email: email,
-        username: username,
-        password: hashpassword,
-      }
-    });
-    console.log('User created successfully');
-    res.send({ status: true, message: 'User created successfully' });
-  }
-  catch (err ){
-    console.log('User not created');
-    res.send({ status: false, message: 'User not created' });
-  }
-
+  userServices.register(req,res);
+  return res;
 });
 
 //Login 
 app.post('/login', async (req, res) => {
+  userServices.login(req,res);
+  return res;
   
-  const { email, password} = req.body;
-   
-  const user = await prisma.users.findFirst({
-    where: {
-      email:email
-    }
-  });
-  
-
-  if (!user) {
-    console.log('user is not registered');
-    return res.json({status: false,message: 'user is not registered'})
-  }
-
-  if ( !email || !password) {
-    console.log('Por favor, preencha todos os campos');
-    return res.json({status: false,message: 'Empty campo'})
-  }
-  
-  const validPassword = await bcrypt.compare(password,user.password)
-
-  if (!validPassword){
-    return res.json({message : "password is incorrect"})
-  }
-
-  const token = jwt.sign({username: user.username, email : user.email}, process.env.KEY, {expiresIn: '3h'})
-  res.cookie('token', token, {httpOnly: true ,maxAge:3*60*60*1000}) //3h em milissegundos
-  return res.json({status: true, message:"login successfully"})
-
 });
 
+//forgot password
 app.post('/forgotPassword', async (req, res) => {
-  const {email} = req.body
-
-  try {
-    const user = await prisma.users.findFirst({
-      where: {
-        email:email
-      }
-    });
-
-    if (!user) {
-      return res.json({message: "user not registered"})
-    }
-
-    const token = jwt.sign({id: user.id}, process.env.KEY, {expiresIn: '1h'})
-
-    var transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: process.env.USER_NAME,
-        pass: process.env.USER_PASSWORD
-      }
-    });
-
-    var mailOptions = {
-      from: process.env.USER_NAME,
-      to: email,
-      subject: 'Reset Password',
-      text: `http://localhost:5173/resetPassword/${token}`
-    };
-
-    transporter.sendMail(mailOptions, function(error, info){
-      if (error) {
-        console.log(error);
-        return res.json({message: "error email not sent"})
-      } else {
-        return res.json({status: true, message: "email  sent"})
-      }
-    }); 
-  }
-  catch (err) {
-    console.log(err);
-  }
-
+  userServices.forgotPassword(req,res);
+  return res;
 })
 
 //reset password
 app.post('/resetPassword/:token', async (req,res) => {
-  const {token} = req.params;
-  const {password} = req.body;
-
-  try {
-    //tem que verificar se o token expirou
-    const decoded =  jwt.verify(token,process.env.KEY)
-    const userId = decoded.id
-    const hashPassword = await bcrypt.hash(password,10)
-
-    await prisma.users.update({
-      where: {
-        id: userId
-      },
-      data: {
-        password: hashPassword // Atualizar o campo de senha
-      }
-    });
-
-    //console.log(user.data);
-    res.json({status: true, message: "ok"})
-
-  }catch (err) {
-    console.log(err);
-    return res.json("invalid token")
-  }
+  userServices.resetPassword(req,res);
+  return res;
 })
 
-const verifyUser = async (req, res, next) => {
-  
-  try {
-    
-    const token = req.cookies.token;
-    
-    if (!token){
-      return res.json({status: false, message : "no token"})
-    }
 
-    const decoded = jwt.verify(token, process.env.KEY)
-    req.user = decoded
-    next()
-
-  }
-  catch (err) {
-    console.error("Error verifying token:", err);
-    return res.json({status: false, message: "UNAUTHORIZED !!!!!"})
-  }
-}
-
-app.get("/verify", verifyUser, (req,res) => {
+app.get("/verify", userServices.verifyUser, (req,res) => {
   return res.json({status: true, message: "authorized", user: req.user})
 })
 
@@ -221,33 +96,23 @@ app.get('/logout', (req,res) => {
 })
 
 app.delete('/delete', async (req, res) => {
-  const token = req.cookies.token;
-  const decoded =  jwt.verify(token,process.env.KEY)
-  
-  try {
-    
-    const user = await prisma.users.findFirst({
-      where: {
-        email:decoded.email
-      }
-    });
-
-    if (!user) {
-      return res.json({status: false, message: "user not exist"})
-    }
-
-    await prisma.users.delete({
-      where: {
-        id:user.id,
-      },
-    });
-
-    res.clearCookie('token')
-    return res.json({status : true, message: 'delete sucess !!!'})
-  }
-  catch (err){
-    console.log(err);
-    return res.json({status : false, message: 'error delete user'})
-  }
-  
+    userServices.deleteUser(req,res);
+    return res;
 })
+
+
+app.get('/films', async (req, res) => {
+  const movies = await movieServices.getAllMovies();
+  res.json(movies);
+
+});
+
+app.get('/films/:id', async (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  if (isNaN(id)) {
+    res.status(400).json({ error: 'Invalid movie ID' });
+    return;
+  }
+  const movie = await movieServices.findMovieById(id);
+  res.json(movie);
+});
